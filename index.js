41ab8c4807d5614befe718c54f88d0448382d725
Arctan <artan.bajqinca@gmail.com>
2024-01-17 15:46:07 +0100

    Add name of all districts & get all resturants of a specific district routing methods

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ app.get("/", function (req, res) {
   }
 });
 
+// get all the names of the districts
+app.get("/districts", function (req, res) {
+  const districtsList = {};
+  for (let i = 0; i < stores.length; i++) {
+    districtsList[stores[i].district] = 1;
+  }
+  res.json(Object.keys(districtsList));
+});
+
+// get all the resturants of a specific district
+app.get("/districts/:disctrictname", (req, res) => {
+  const { disctrictname } = req.params;
+  const districtStores = stores.filter(
+    (store) => store.district === disctrictname
+  );
+  res.json(districtStores);
+});
+
 app.post(
   "/",
   express.json(), // for parsing application/json body in POST
@@ -28,7 +46,7 @@ app.post(
 app.delete("/", function (req, res) {
   const { storename } = req.query;
   console.log(storename);
-  const index = stores.findIndex((store) => store.name === storename);
+  const index = stores.findIndex((stores) => stores.name === storename);
   if (index > -1) {
     stores.splice(index, 1);
     res.send(`Store found! Deleting store with index: ${index}`);
