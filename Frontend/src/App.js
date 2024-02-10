413ff3bd9d85cc8b8599e1dbee175a4cece365e3
Lavdim Ismaili <lavdim_milan@hotmail.com>
2024-02-10 16:52:11 +0100

    Updated frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,68 @@
-import logo from "./logo.svg";
-import "./App.css";
-// import axios from 'axios';
-
-// function await fetchData() {
-//   const data = await axios.get('https//localhost:3000/')
-//   setData
-// }
+import React from 'react';
+import './App.css'; 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="header">
-      <div className="navbar-section"></div>
-    </div>
+    <>
+      <Container fluid style={{ 
+        backgroundImage: `url(${process.env.PUBLIC_URL}/img/gym1.png)`,
+        backgroundSize: 'cover',
+        padding: 0,
+        margin: 0,
+        height: "50vh"
+      }}>
+        <CustomNavbar/>
+      </Container>
+
+
+      <Container fluid style={{
+        padding: 0,
+        margin: 0,
+        backgroundColor: "black",
+        height: "6vh" 
+      }}>
+      </Container>
+
+      <Container fluid style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh"  
+      }}>
+      </Container>
+    </>
   );
 }
+
+function CustomNavbar() {
+  return (
+    <Navbar expand="lg" style={{ backgroundColor: "transparent" }}>
+      <Container>
+      <Navbar.Brand href="#home" className='text'>jkpgcity.</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className='text' >Home</Nav.Link>
+            <Nav.Link href="#link" className='text'>Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown" >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
 
 export default App;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"; // Global styles
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
