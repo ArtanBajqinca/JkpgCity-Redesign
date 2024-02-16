09aacc2013e6831675368ec2252d5a9daf1b0dbf
Lavdim Ismaili <lavdim_milan@hotmail.com>
2024-02-17 00:41:18 +0100

    UI updated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,64 +1,69 @@
-import React from 'react';
-import './App.css'; 
+import React, { useRef, useEffect } from 'react';
+import './App.css';
+import './Font.css';  
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { GrMenu } from "react-icons/gr";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <>
-      <Container fluid style={{ 
-        backgroundImage: `url(${process.env.PUBLIC_URL}/img/gym1.png)`,
-        backgroundSize: 'cover',
-        padding: 0,
-        margin: 0,
-        height: "50vh"
-      }}>
-        <CustomNavbar/>
-        <Container style={{ paddingLeft: 220, paddingTop: 40}}>
-          <h1 style={{color: "white", fontFamily: "Acumin Pro Regular", fontWeight: 'bolder', fontSize: 60}}>
-            REKREATION
-          </h1>
-          <h2 style={{color: "white", fontFamily: "Acumin Pro Regular", fontWeight: 'lighter', fontSize: 16, width: 400, paddingLeft: 4, paddingTop: 30,}}>
-          I Jönköpings stadskärna hittar du allt för att må bra, med ett stort utbud av olika gym, frisörer och salonger - allt för att skämma bort dig själv.
-          </h2>
-        </Container>
-      </Container>
 
-      <Container fluid style={{
-        padding: 0,
-        margin: 0,
-        backgroundColor: "black",
-        height: "6vh" 
-      }}>
-      <SpinningRow/>
-      </Container>
+      <FirstSection/>
 
-      <Container fluid style={{
-        padding: 0,
-        margin: 0,
-        height: "100vh"  
-      }}>
-      </Container>
+      <SecondSection/>
+
+      <ThirdSection/>
+
     </>
   );
 }
 
+function FirstSection() {
+
+  return (
+
+    <div className='firstSectionBody'>
+
+    <CustomNavbar/>
+
+    <div className='firstSectionContentDiv'>
+
+      <h1>
+        REKREATION
+      </h1>
+
+      <h2>
+        I Jönköpings stadskärna hittar du allt för att må bra, med ett stort utbud av olika gym, frisörer och salonger - allt för att skämma bort dig själv.
+      </h2>
+
+    </div>
+
+    </div>
+
+  )
+
+}
+
 function CustomNavbar() {
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "transparent" }}>
-      <Container>
-        <Navbar.Brand href="#home" className='text' style={{ color: "white", fontFamily: "Acumin Pro Regular", fontWeight: 'bold', fontSize: 40, paddingLeft: 210, paddingRight: 340}}>jkpgcity.</Navbar.Brand>
+    <Navbar expand="lg" style={{ backgroundColor: "transparent", margin:0}}>
+      <Container style={{ padding:0,margin:0}}>
+        <Navbar.Brand href="#home" style={{ paddingLeft: 300, paddingRight: 275}}>
+          <img
+            src= "./img/jkpgcity.svg"
+            height="40"
+            alt="jkpgcity logo"
+          />
+        </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className='text' style={{ color: "black", fontSize: 16, backgroundColor: 'white', fontFamily: "Acumin Pro Regular"}}>UPPTÄCK</Nav.Link>
-            <Nav.Link href="#link" className='text' style={{ color: "white", fontSize: 16, fontFamily: "Acumin Pro Regular" }}>EVENEMANG</Nav.Link>
-            <Nav.Link href="#link" className='text' style={{ color: "white", fontSize: 16, fontFamily: "Acumin Pro Regular" }}>STADSDELAR</Nav.Link>
-            <Nav.Link href="#link" className='text' style={{ color: "white", fontSize: 16, fontFamily: "Acumin Pro Regular" }}>INFORMATION</Nav.Link>
+            <Nav.Link href="#home" className='text' style={{ color: "black", fontSize: 16, fontWeight: 'bold', backgroundColor: 'white', fontFamily: "Acumin Pro"}}>UPPTÄCK</Nav.Link>
+            <Nav.Link href="#link" className='text' style={{ color: "white", fontSize: 16, fontWeight: 'bold', fontFamily: "Acumin Pro" }}>EVENEMANG</Nav.Link>
+            <Nav.Link href="#link" className='text' style={{ color: "white", fontSize: 16, fontWeight: 'bold', fontFamily: "Acumin Pro" }}>STADSDELAR</Nav.Link>
+            <Nav.Link href="#link" className='text' style={{ color: "white", fontSize: 16, fontWeight: 'bold', fontFamily: "Acumin Pro" }}>INFORMATION</Nav.Link>
             <NavDropdown title={<GrMenu  style={{ fontSize: "24px", color: "white" }} />} id="basic-nav-dropdown" >
               <NavDropdown.Item href="#action/3.1">Dropdown Link 1</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Dropdown Link 2</NavDropdown.Item>
@@ -71,26 +76,24 @@ function CustomNavbar() {
   );
 }
 
-function SpinningRow() {
+function SecondSection() {
   return (
-    <Container fluid className="spinning-row">
-      <div className="spinning-items">
-        {[...Array(2)].map((_, index) => (
-          <React.Fragment key={index}>
-            <div className="spinning-item">POGGGGGGGGGG</div>
-            <div className="spinning-item">SUCKKKKKKKKk</div>
-            <div className="spinning-item">DOGGGGGGGGGG</div>
-            <div className="spinning-item">LUCCCKKKKKKK</div>
-            <div className="spinning-item">MUUUUUUUCKKK</div>
-            <div className="spinning-item">JUUUUCKKKKKK</div>
-            <div className="spinning-item">FUDGEEEEEEEE</div>
-            <div className="spinning-item">CUUUUUUUUUUH</div>
-          </React.Fragment>
-        ))}
-      </div>
-    </Container>
-  );
+    <div className='secondSectionBody'>
+      <img
+        src = "./img/jkpgDistricts.svg"
+        height = "28"
+        alt = "jkpgcity districts"
+      />
+    </div>
+  )
 }
 
+function ThirdSection() {
+  return (
+    <div className='thirdSectionBody'>
+      
+    </div>
+  )
+}
 
 export default App;
